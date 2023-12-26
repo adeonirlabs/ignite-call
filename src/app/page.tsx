001a1d7d34911b2653bc1dc8e4f782c963d2e3c1
Adeonir Kohl <adeonir@gmail.com>
2023-12-26 16:31:44 -0300

    Install and setup flowbite library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from 'flowbite-react'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2 className="mb-3 text-2xl font-semibold">Create Next App</h2>
+      <Button>Click me</Button>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,14 @@ import type { Config } from 'tailwindcss'
 const config: Config = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 }
 
 export default config
