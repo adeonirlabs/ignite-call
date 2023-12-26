08b0de38ab1a6cece97dc18d9b9698edf623c07a
Adeonir Kohl <adeonir@gmail.com>
2023-12-26 20:45:25 -0300

    Add image and update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image'
+
+import preview from '~/assets/images/preview.png'
+
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2 className="mb-3 text-2xl font-medium">Create Next App</h2>
+    <main className="ml-auto flex h-screen max-w-[calc(100vw_-_(100vw_-_72rem)_/_2)] items-center gap-20">
+      <section className="flex flex-col gap-4 p-10">
+        <h1 className="text-4xl font-bold md:text-6xl">Agendamento descomplicado</h1>
+        <p className="text-base md:text-xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.
+        </p>
+      </section>
+      <section className="hidden overflow-hidden md:flex">
+        <Image
+          alt="Calendário mostrando o funcionamento da aplicação"
+          className="max-w-none shrink-0"
+          height={400}
+          priority
+          quality={100}
+          src={preview}
+        />
+      </section>
     </main>
   )
 }
