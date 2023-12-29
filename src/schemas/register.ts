2e1cd267cd9c13a4bcfbc9750938350b993e36f7
Adeonir Kohl <adeonir@gmail.com>
2023-12-29 09:58:26 -0300

    Update register schema validation

--- src/schemas/register.ts
+++ src/schemas/register.ts
@@ -4,9 +4,12 @@ const register = {
   username: z
     .string()
     .min(3, 'O nome de usuário deve ter pelo menos 3 caracteres.')
-    .regex(/^[a-z\\-]*[a-z]$/i, 'O nome de usuário deve conter apenas letras e hifens.')
+    .regex(/^[a-z\\-]*[a-z]$/i, 'O nome de usuário deve conter apenas letras e hifens e terminar com uma letra.')
     .transform(value => value.toLowerCase()),
-  fullName: z.string().min(8, 'O nome completo deve ter pelo menos 8 caracteres.'),
+  fullName: z
+    .string()
+    .min(3, 'O nome completo deve ter pelo menos 3 caracteres.')
+    .regex(/\s/, 'O nome completo deve conter pelo menos um espaço em branco.'),
 }
 
 export const registerSchema = z.object(register)
