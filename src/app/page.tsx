b6ae1f2dc9f9a381263ab443322a7877fcdb5276
Adeonir Kohl <adeonir@gmail.com>
2024-01-01 23:48:41 -0300

    Update styles on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
     <main className="ml-auto flex h-screen max-w-[calc(100vw_-_(100vw_-_72rem)_/_2)] items-center gap-20">
       <section className="flex flex-col gap-4 p-10">
         <h1 className="text-4xl font-bold md:text-6xl">Agendamento descomplicado</h1>
-        <p className="text-base md:text-xl">
+        <p className="mb-4 text-base md:text-xl">
           Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.
         </p>
-        <ClaimUsernameForm className="mt-4" />
+        <ClaimUsernameForm />
       </section>
       <section className="hidden overflow-hidden md:flex">
         <Image
