7e0934d335250bd098c8433eb7c81fd89011c24f
Adeonir Kohl <adeonir@gmail.com>
2024-01-02 09:26:41 -0300

    Update connect button based on session status

--- src/app/register/connect/page.tsx
+++ src/app/register/connect/page.tsx
@@ -1,9 +1,16 @@
+'use client'
+
 import { ArrowRight } from 'lucide-react'
+import { useSession } from 'next-auth/react'
 
 import { ConnectToGoogleForm } from '~/components/forms/connect-to-google'
 import { Steps } from '~/components/ui/steps'
 
 export default function Connect() {
+  const session = useSession()
+
+  const isSignedIn = session.status === 'authenticated'
+
   return (
     <main className="mx-auto mb-4 mt-20 max-w-xl px-4">
       <header className="flex flex-col gap-6 pb-6">
@@ -18,7 +25,7 @@ export default function Connect() {
       </header>
       <section className="flex flex-col gap-4 rounded-lg border border-zinc-200/10 bg-zinc-600/20 p-4">
         <ConnectToGoogleForm />
-        <button className="btn-accent btn" disabled type="submit">
+        <button className="btn-accent btn" disabled={!isSignedIn} type="submit">
           Próximo passo
           <ArrowRight />
         </button>
--- src/components/forms/connect-to-google.tsx
+++ src/components/forms/connect-to-google.tsx
@@ -1,21 +1,35 @@
 'use client'
 
-import { ArrowRight } from 'lucide-react'
+import { ArrowRight, Check } from 'lucide-react'
 import { useSearchParams } from 'next/navigation'
-import { signIn } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 
 export function ConnectToGoogleForm() {
   const params = useSearchParams()
+  const session = useSession()
+
   const hasAuthError = !!params.get('error')
+  const isSignedIn = session.status === 'authenticated'
+
+  const handleConnect = async () => {
+    await signIn('google', { callbackUrl: '/register/connect' })
+  }
 
   return (
     <div>
       <div className="flex items-center justify-between rounded-lg border border-zinc-200/10 p-4">
         <strong>Google Agenda</strong>
-        <button className="btn-accent btn-outline btn btn-sm" onClick={() => signIn('google')} type="submit">
-          Conectar
-          <ArrowRight />
-        </button>
+        {isSignedIn ? (
+          <button className="btn-accent btn-outline btn btn-sm" disabled={isSignedIn} type="submit">
+            Conectado
+            <Check size={16} />
+          </button>
+        ) : (
+          <button className="btn-accent btn-outline btn btn-sm" onClick={handleConnect} type="submit">
+            Conectar
+            <ArrowRight size={16} />
+          </button>
+        )}
       </div>
       {hasAuthError && (
         <span className="label-error label">
