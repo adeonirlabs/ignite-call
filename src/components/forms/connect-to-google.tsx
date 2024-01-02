462a839c543022b5f0a25f9c8cfef2301961740e
Adeonir Kohl <adeonir@gmail.com>
2024-01-02 09:09:17 -0300

    Add permission error on connect to google

--- src/components/forms/connect-to-google.tsx
+++ src/components/forms/connect-to-google.tsx
@@ -1,16 +1,28 @@
 'use client'
 
 import { ArrowRight } from 'lucide-react'
+import { useSearchParams } from 'next/navigation'
 import { signIn } from 'next-auth/react'
 
 export function ConnectToGoogleForm() {
+  const params = useSearchParams()
+  const hasAuthError = !!params.get('error')
+
   return (
-    <div className="flex items-center justify-between rounded-lg border border-zinc-200/10 p-4">
-      <strong>Google Agenda</strong>
-      <button className="btn-accent btn-outline btn btn-sm" onClick={() => signIn('google')} type="submit">
-        Conectar
-        <ArrowRight />
-      </button>
+    <div>
+      <div className="flex items-center justify-between rounded-lg border border-zinc-200/10 p-4">
+        <strong>Google Agenda</strong>
+        <button className="btn-accent btn-outline btn btn-sm" onClick={() => signIn('google')} type="submit">
+          Conectar
+          <ArrowRight />
+        </button>
+      </div>
+      {hasAuthError && (
+        <span className="label-error label">
+          Erro ao conectar com o Google Agenda. <br />
+          Verifique se você habilitou as permissões de acesso ao calendário.
+        </span>
+      )}
     </div>
   )
 }
