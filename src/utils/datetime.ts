b97a42b40cd06ebcf0fd245f5682e103adea36f0
Adeonir Kohl <adeonir@gmail.com>
2024-01-04 11:51:19 -0300

    Create utils function to convert a time to minutes

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -3,3 +3,8 @@ export const getWeekDays = () => {
     .map(day => new Intl.DateTimeFormat('pt-BR', { weekday: 'long' }).format(new Date(2021, 0, day + 3)))
     .map(day => day.charAt(0).toUpperCase() + day.slice(1))
 }
+
+export const convertTimeToMinutes = (time: string) => {
+  const [hours, minutes] = time.split(':').map(Number)
+  return hours * 60 + minutes
+}
