3c15dc52967b366a1b8eb0de7988ee78c94b2877
Adeonir Kohl <adeonir@gmail.com>
2024-01-22 21:56:58 -0300

    Update next auth type definition

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,9 +1,11 @@
 import NextAuth from 'next-auth'
 
-declare module 'next-auth' {
+declare module '@auth/core/types' {
   interface User {
     username: string
   }
+}
+declare module 'next-auth' {
   interface Session {
     user: User
   }
