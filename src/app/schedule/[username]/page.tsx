4fd041af13e33e254a2f2d8f560a929f9aeafb34
Adeonir Kohl <adeonir@gmail.com>
2024-01-23 08:26:22 -0300

    Add user avatar and description to schedule page

--- src/app/schedule/[username]/page.tsx
+++ src/app/schedule/[username]/page.tsx
@@ -1,3 +1,23 @@
-export default function Schedule() {
-  return <div>Schedule</div>
+import Image from 'next/image'
+
+import { auth } from '~/auth'
+
+export default async function Schedule() {
+  const session = await auth()
+
+  return (
+    <main className="max-w-4xl- mx-auto mb-4 mt-20 px-4">
+      <header className="flex flex-col items-center">
+        <Image
+          alt="Adeonir Kohl"
+          className="mb-2 h-24 w-24 rounded-full"
+          height={200}
+          src="https://github.com/adeonir.png"
+          width={200}
+        />
+        <h2 className="text-xl font-medium">{session?.user.name}</h2>
+        <span className="mt-1 text-xs text-zinc-400">{session?.user.bio}</span>
+      </header>
+    </main>
+  )
 }
