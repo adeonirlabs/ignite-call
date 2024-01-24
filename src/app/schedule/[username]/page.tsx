d3e2a7dc765fb4d3fc5e3a7ec2fb83460b23344e
Adeonir Kohl <adeonir@gmail.com>
2024-01-24 08:53:56 -0300

    Rename schedule form to select date time form

--- src/app/schedule/[username]/page.tsx
+++ src/app/schedule/[username]/page.tsx
@@ -1,14 +1,14 @@
 import Image from 'next/image'
 
 import { auth } from '~/auth'
-import { ScheduleForm } from '~/components/forms/schedule'
+import { SelectDateTimeForm } from '~/components/forms/select-datetime'
 
 export default async function Schedule() {
   const session = await auth()
 
   return (
     <main className="mx-auto mb-4 mt-20 flex w-full max-w-4xl flex-col items-center px-4">
-      <header className="flex flex-col items-center justify-center">
+      <header className="mb-12 flex flex-col items-center justify-center">
         <Image
           alt="Adeonir Kohl"
           className="mb-2 h-24 w-24 rounded-full"
@@ -20,7 +20,7 @@ export default async function Schedule() {
         <h2 className="text-xl font-medium">{session?.user.name}</h2>
         <span className="mt-1 text-xs text-zinc-400">{session?.user.bio}</span>
       </header>
-      <ScheduleForm className="mt-12" />
+      <SelectDateTimeForm />
     </main>
   )
 }
--- src/components/forms/select-datetime.tsx
+++ src/components/forms/select-datetime.tsx
@@ -7,19 +7,20 @@ import { cn } from '~/utils/classnames'
 
 import { TimePicker } from '../ui/timepicker'
 
-export function ScheduleForm({ className, ...props }: ComponentProps<'section'>) {
+export function SelectDateTimeForm({ className, ...props }: ComponentProps<'section'>) {
   const isDateSelected = true
 
   return (
     <section
       className={cn(
-        'relative grid rounded-lg border border-zinc-200/10 bg-zinc-600/20',
+        'relative grid w-full rounded-lg border border-zinc-200/10 bg-zinc-600/20',
         isDateSelected ? 'grid-cols-1 md:grid-cols-[1fr_20rem]' : 'max-w-xl grid-cols-1',
         className,
       )}
       {...props}
     >
       <Calendar className="p-6" />
+
       {isDateSelected ? (
         <TimePicker
           className={cn(
