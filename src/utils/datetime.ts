cbf6283df4738022e308672b47a922379746a701
Adeonir Kohl <adeonir@gmail.com>
2024-01-23 21:11:07 -0300

    Update getWeekDays function to return a short version

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -1,7 +1,11 @@
-export const getWeekDays = () => {
+interface WeekDaysParams {
+  short?: boolean
+}
+
+export const getWeekDays = ({ short = false }: WeekDaysParams = {}) => {
   return Array.from(Array(7).keys())
     .map(day => new Intl.DateTimeFormat('pt-BR', { weekday: 'long' }).format(new Date(2021, 0, day + 3)))
-    .map(day => day.charAt(0).toUpperCase() + day.slice(1))
+    .map(day => (short ? day.charAt(0).toUpperCase() + day.slice(1, 3) : day.charAt(0).toUpperCase() + day.slice(1)))
 }
 
 export const convertTimeToMinutes = (time: string) => {
