1e9b00f38f3329afe0b314a215a19673fc792f67
Adeonir Kohl <adeonir@gmail.com>
2024-01-23 21:12:03 -0300

    Add schedule form to schedule page

--- src/app/schedule/[username]/page.tsx
+++ src/app/schedule/[username]/page.tsx
@@ -1,23 +1,26 @@
 import Image from 'next/image'
 
 import { auth } from '~/auth'
+import { ScheduleForm } from '~/components/forms/schedule'
 
 export default async function Schedule() {
   const session = await auth()
 
   return (
-    <main className="max-w-4xl- mx-auto mb-4 mt-20 px-4">
-      <header className="flex flex-col items-center">
+    <main className="mx-auto mb-4 mt-20 flex w-full max-w-[52rem] flex-col items-center px-4">
+      <header className="flex flex-col items-center justify-center">
         <Image
           alt="Adeonir Kohl"
           className="mb-2 h-24 w-24 rounded-full"
           height={200}
+          priority
           src="https://github.com/adeonir.png"
           width={200}
         />
         <h2 className="text-xl font-medium">{session?.user.name}</h2>
         <span className="mt-1 text-xs text-zinc-400">{session?.user.bio}</span>
       </header>
+      <ScheduleForm className="mt-12" />
     </main>
   )
 }
