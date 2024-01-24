5a7e2d026c5aeae17cd78a99cdf5d7825b88573d
Adeonir Kohl <adeonir@gmail.com>
2024-01-24 17:37:08 -0300

    Add useForm to confirm schedule

--- src/components/forms/confirm-schedule.tsx
+++ src/components/forms/confirm-schedule.tsx
@@ -1,10 +1,25 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowLeft, ArrowRight, Calendar, Clock } from 'lucide-react'
 import type { ComponentProps } from 'react'
+import { useForm } from 'react-hook-form'
 
+import { type ConfirmSchedule, confirmScheduleSchema } from '~/schemas/confirm-schedule'
 import { cn } from '~/utils/classnames'
 
 export function ConfirmScheduleForm({ className, ...props }: ComponentProps<'section'>) {
-  const isSubmitting = false
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<ConfirmSchedule>({
+    resolver: zodResolver(confirmScheduleSchema),
+  })
+
+  const onSubmit = async (data: ConfirmSchedule) => {
+    console.info(data)
+  }
 
   return (
     <section
@@ -22,14 +37,16 @@ export function ConfirmScheduleForm({ className, ...props }: ComponentProps<'sec
         </div>
       </header>
 
-      <form className="flex flex-col gap-4 p-6">
+      <form className="flex flex-col gap-4 p-6" onSubmit={handleSubmit(onSubmit)}>
         <label className="form-control w-full">
           <span className="label label-text">Nome completo</span>
           <input
             className={cn('input input-bordered input-accent w-full placeholder:text-zinc-300/50')}
             placeholder="Seu nome"
             type="text"
+            {...register('name')}
           />
+          {errors.name ? <span className="label-error label">{errors.name.message}</span> : null}
         </label>
         <label className="form-control w-full">
           <span className="label label-text">Endereço de e-mail</span>
@@ -37,7 +54,9 @@ export function ConfirmScheduleForm({ className, ...props }: ComponentProps<'sec
             className={cn('input input-bordered input-accent w-full placeholder:text-zinc-300/50')}
             placeholder="seu@email.com"
             type="email"
+            {...register('email')}
           />
+          {errors.email ? <span className="label-error label">{errors.email.message}</span> : null}
         </label>
         <label className="form-control w-full">
           <span className="label label-text">Observações</span>
@@ -45,7 +64,9 @@ export function ConfirmScheduleForm({ className, ...props }: ComponentProps<'sec
             className="textarea textarea-bordered textarea-accent placeholder:text-zinc-300/50"
             placeholder="Anote alguma observação sobre a reunião"
             rows={5}
+            {...register('comments')}
           />
+          {errors.comments ? <span className="label-error label">{errors.comments.message}</span> : null}
         </label>
         <div className="flex items-center justify-end gap-4">
           <button className="btn btn-outline mt-2" disabled={isSubmitting} type="button">
