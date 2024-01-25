4e5e1c408b328701a36c62af9d1bf0409918de64
Adeonir Kohl <adeonir@gmail.com>
2024-01-25 18:35:46 -0300

    Migrate from sqlite to mysql

--- prisma/migrations/20240104214001_create_time_intervals_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "time_intervals" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "user_id" TEXT NOT NULL,
-    "week_day" INTEGER NOT NULL,
-    "start_time" INTEGER NOT NULL,
-    "end_time" INTEGER NOT NULL,
-    CONSTRAINT "time_intervals_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
