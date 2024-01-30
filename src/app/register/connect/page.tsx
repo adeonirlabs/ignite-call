959b6714cbc1f08583f2fba269c967abbdbb01b4
Adeonir Kohl <adeonir@gmail.com>
2024-01-30 10:04:03 -0300

    Refactor connect page and move client side stuff to connect to google form

--- src/app/register/connect/page.tsx
+++ src/app/register/connect/page.tsx
@@ -1,30 +1,7 @@
-'use client'
-
-import { ArrowRight } from 'lucide-react'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { useSession } from 'next-auth/react'
-import { useState } from 'react'
-
 import { ConnectToGoogleForm } from '~/components/forms/connect-to-google'
 import { Steps } from '~/components/ui/steps'
-import { sleepTime } from '~/utils/sleep'
 
 export default function Connect() {
-  const params = useSearchParams()
-  const session = useSession()
-  const router = useRouter()
-
-  const hasAuthError = !!params.get('error')
-  const isAuthenticated = session.status === 'authenticated'
-
-  const [isSubmitting, setIsSubmitting] = useState(false)
-
-  const handleNextStep = () => {
-    setIsSubmitting(true)
-    sleepTime(500)
-    router.push(`/register/agenda`)
-  }
-
   return (
     <main className="mx-auto mb-4 mt-20 max-w-xl px-4">
       <header className="flex flex-col gap-6 pb-6">
@@ -39,21 +16,6 @@ export default function Connect() {
       </header>
       <section className="flex flex-col gap-4 rounded-lg border border-zinc-200/10 bg-zinc-600/20 p-4">
         <ConnectToGoogleForm />
-        <button
-          className="btn btn-accent"
-          disabled={!isAuthenticated || hasAuthError || isSubmitting}
-          onClick={handleNextStep}
-          type="submit"
-        >
-          {isSubmitting ? (
-            <span className="loading loading-spinner"></span>
-          ) : (
-            <>
-              Próximo passo
-              <ArrowRight />
-            </>
-          )}
-        </button>
       </section>
     </main>
   )
--- src/components/forms/connect-to-google.tsx
+++ src/components/forms/connect-to-google.tsx
@@ -1,29 +1,61 @@
 'use client'
 
 import { ArrowRight, Check } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { signIn, useSession } from 'next-auth/react'
+import { useState } from 'react'
+
+import { sleepTime } from '~/utils/sleep'
 
 export function ConnectToGoogleForm() {
   const session = useSession()
+  const params = useSearchParams()
+  const router = useRouter()
 
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const hasAuthError = !!params.get('error')
   const isAuthenticated = session.status === 'authenticated'
 
   const handleConnect = async () => {
     await signIn('google')
   }
 
+  const handleNextStep = () => {
+    setIsSubmitting(true)
+    sleepTime(500)
+    router.push(`/register/agenda`)
+  }
+
   return (
-    <form className="flex items-center justify-between rounded border border-zinc-200/10 p-4">
-      <strong>Google Agenda</strong>
+    <>
+      <form className="flex items-center justify-between rounded border border-zinc-200/10 p-4">
+        <strong>Google Agenda</strong>
+        <button
+          className="btn btn-outline btn-accent btn-sm"
+          disabled={isAuthenticated}
+          onClick={isAuthenticated ? undefined : handleConnect}
+          type="button"
+        >
+          {isAuthenticated ? 'Conectado' : 'Conectar'}
+          {isAuthenticated ? <Check size={16} /> : <ArrowRight size={16} />}
+        </button>
+      </form>
       <button
-        className="btn btn-outline btn-accent btn-sm"
-        disabled={isAuthenticated}
-        onClick={isAuthenticated ? undefined : handleConnect}
-        type="button"
+        className="btn btn-accent"
+        disabled={!isAuthenticated || hasAuthError || isSubmitting}
+        onClick={handleNextStep}
+        type="submit"
       >
-        {isAuthenticated ? 'Conectado' : 'Conectar'}
-        {isAuthenticated ? <Check size={16} /> : <ArrowRight size={16} />}
+        {isSubmitting ? (
+          <span className="loading loading-spinner"></span>
+        ) : (
+          <>
+            Próximo passo
+            <ArrowRight />
+          </>
+        )}
       </button>
-    </form>
+    </>
   )
 }
